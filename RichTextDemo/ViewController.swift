102ac4f77b5348037d039e31b98773ac8308a0c5
yaya <hejiaqi.yaya@bytedanc.com>
2021-06-16 19:43:32 +0800

    Initial Commit

---
+++ RichTextDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RichTextDemo
+//
+//  Created by 改这里为你的大名 on 2021/6/16.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RichTextDemoTests/RichTextDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  RichTextDemoTests.swift
+//  RichTextDemoTests
+//
+//  Created by 改这里为你的大名 on 2021/6/16.
+//  
+
+
+import XCTest
+@testable import RichTextDemo
+
+class RichTextDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
